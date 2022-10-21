ad445810fc21ce6ac692823cfca0a742a38747a6
Lim-SeongJun <dlatjdwns331@naver.com>
2022-10-21 10:26:48 +0900

    [DEL]garbage files

--- web/static.py
+++
@@ -1,8 +0,0 @@
-import os
-from kb import settings
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(settings.BASE_DIR, 'web', 'static')
-]
-
-STATIC_ROOT = os.path.join(settings.BASE_DIR, 'static')
